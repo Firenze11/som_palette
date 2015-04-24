9d694491708101858f6a95f65d76634380153873
Lezhi Li <lli2@gsd.harvard.edu>
2015-04-24 14:14:24 -0400

    learning function finished

---
+++ classes.py
@@ -0,0 +1,98 @@
+
+# coding: utf-8
+
+# In[3]:
+
+from PIL import Image
+
+class Dataset:
+    def __init__ (self, filename):
+        im = Image.open(filename,'r') #Can be many different formats.
+        pix = im.load()
+        print im.size #Get the width and hight of the image for iterating over
+        im.show()
+        
+        self._vectors = []
+        self._labels = []
+        
+        for j in xrange(im.size[1]):
+            for i in xrange(im.size[0]):
+                self._vectors.append(pix[i,j])
+                self._labels.append((i,j))
+
+    def __len__ (self):
+        return len (self._vectors)
+    
+    def __str__ (self):
+        return str (self._vectors)
+
+    def __getitem__ (self, key):
+        return (self._vectors[key], self._labels[key])
+
+    def vectors (self):
+        return self._vectors
+    
+    def labels (self):
+        return self._labels
+    
+    def get_vector (self, point):
+        return self._vectors[point]
+    
+    def get_label (self, point):
+        return self._labels[point]
+
+a = Dataset("cat.jpeg")
+print a[4]
+print a.get_vector(4)
+print a.get_label(4)
+
+
+
+class Node:
+    def __init__ (self, _v, _x, _y):
+        self.v = _v
+        self.x = _x
+        self.y = _y
+    
+    def __str__ (self):
+        return str (self.v)
+    
+    def set_v (self, v):
+        self.v = v
+        
+    def get_v (self):
+        return self.v
+        
+    def set_pos (self, pos):
+        self.x = pos[0]
+        self.y = pos[1]
+        
+    def get_pos (self):
+        return (self.x, self.y)
+
+'''    
+class SOM:
+    def __init__ (self, w, h)
+        self.node = []
+        for i in range(h):
+            self.node.append([])
+            for j in range(w):
+                temp_node = new Node((random.randint(0,255), random.randint(0,255), random.randint(0,255)), j, i)
+                self.node[i].append(temp_node)
+                
+    
+    def __len__ (self):
+        return (len (self.node), len (self.node[0])
+    
+    def __str__ (self):
+        return str ("som")
+
+    def __getitem__ (self, i, j):
+        return (self.node[i][j])
+'''
+
+
+# In[ ]:
+
+
+
