2f9cc54a8cf29a499f4791bb51d35c17393f6be8
spyridon-amp <sampanavos@gsd.harvard.edu>
2015-04-27 19:18:10 -0400

    run

---
+++ run_som.py
@@ -0,0 +1,12 @@
+import functions
+import classes
+
+s_o_m = functions.initialize_som()
+
+functions.draw(s_o_m)
+
+image = classes.Dataset("sample.bmp")
+
+functions.train(image,s_o_m)
+
+functions.draw(s_o_m)
