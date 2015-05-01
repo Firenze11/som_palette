c021a2892689ed008bfb25818307d4a6fb2057b2
Ting Zhou <tingzhou@college.harvard.edu>
2015-04-30 23:47:37 -0400

    final

--- submission/code/run_som.py
+++ submission/code/run_som.py
@@ -1,11 +1,9 @@
 import functions
 import classes
 
-pic_file = "../data/wisconsin.jpg"
+pic_file = "../data/dianaelephant2.jpg"
 
 s_o_m = functions.initialize_som()
-
-functions.draw(s_o_m)
 
 image = classes.Dataset(pic_file)
 
